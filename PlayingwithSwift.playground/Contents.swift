278b563775ca5f56e88696ec6e415dd247cb8200
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-09-25 08:36:36 -0600

    Added variables and let and played around

--- PlayingwithSwift.playground/Contents.swift
+++ PlayingwithSwift.playground/Contents.swift
@@ -2,4 +2,38 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//Explicit type declaration with no value
+var ramen : String
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge * 3
+var someValue = 3.1415 * 56
+someValue = someValue * 234.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName : String
+myName = "Mitch"
+print(myName)
+//myName = soup
+// Can't assign into a let once assigned.
+var sayMyName : String
+sayMyName = "mitch"
+print (sayMyName)
+print(" Say My Name")
+var whatsMyAgeAgain : Int
+whatsMyAgeAgain = 20
+print (whatsMyAgeAgain)
+whatsMyAgeAgain = whatsMyAgeAgain * 9
+print(whatsMyAgeAgain)
+
+
+
+
